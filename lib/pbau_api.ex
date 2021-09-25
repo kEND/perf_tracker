1511a4f55061d0ae6d3acee3dbd5ca8bd972ee11
Ken Barker <ken.barker@gmail.com>
2021-09-25 16:32:55 +0000

    add name/0 to PbauApi

--- lib/pbau_api.ex
+++ lib/pbau_api.ex
@@ -1,15 +1,21 @@
-defmodule PbauQuartusApi do
+defmodule PbauApi do
   alias Poison.Parser
 
   use Tesla, only: [:post], docs: false
 
-  plug(Tesla.Middleware.BaseUrl, "http://pbauquartus.painlesspm.com:4647")
+  plug(Tesla.Middleware.BaseUrl, "http://pbau.painlesspm.com:4647")
   plug(Tesla.Middleware.JSON)
 
   def in_consensus?() do
     {:ok, %{body: body}} = post("/", %{jsonrpc: "2.0", id: 1, method: "info_in_consensus"}, [])
     %{"result" => %{"in_consensus" => in_consensus}} = Parser.parse!(body)
     in_consensus
+  end
+
+  def name() do
+    {:ok, %{body: body}} = post("/", %{jsonrpc: "2.0", id: 1, method: "info_name"}, [])
+    %{"result" => %{"name" => name}} = Parser.parse!(body)
+    name
   end
 
   def height do
